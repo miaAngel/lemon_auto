a133376d9fb7f817e916aa853b28da956f4e96dc
qibei <qibei@persagy.com>
2021-06-30 14:28:28 +0800

    配置文件update

--- src/test/java/com/lemon/data/Constants.java
+++ src/test/java/com/lemon/data/Constants.java
@@ -6,9 +6,9 @@ public class Constants {
     //Excel文件的路径
     public static final String EXCEL_FILE_PATH=System.getProperty("user.dir")+"/src/test/resources/api_testcases_futureloan_v4.xls";
     //接口BaseUrl地址
-    public static final String BASE_URI="http://8.129.52.205:8012/futureloan";
+    public static final String BASE_URI="http://123.57.194.183:8012/futureloan";
     //数据库baseuri
-    public static final String DB_BASE_URI="8.129.52.205:33060";
+    public static final String DB_BASE_URI="123.57.194.183:33060";
     //数据库名
     public static final String DB_NAME="futureloan";
     public static final String DB_USERNAME="root";
